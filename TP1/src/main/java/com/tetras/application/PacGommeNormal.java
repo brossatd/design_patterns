8f2afa84a3bd077f66425411355e63b4d9365b88
brossatd <87855944+brossatd@users.noreply.github.com>
2022-01-25 13:34:24 +0100

    TP1

---
+++ TP1/src/main/java/com/tetras/application/PacGommeNormal.java
@@ -0,0 +1,14 @@
+package com.tetras.application;
+
+public class PacGommeNormal implements IPacGomme {
+
+    public PacGommeNormal(){}
+    
+    public int getTaille(){
+        return taille;
+    }
+    public String getCouleur(){
+        return couleur;
+    } 
+
+}
---
+++ TP1/src/main/java/com/tetras/application/Pacman.java
@@ -0,0 +1,14 @@
+package com.tetras.application;
+
+public class Pacman {
+
+    private static Pacman instance = new Pacman();
+
+    private Pacman(){}
+
+    public static Pacman getInstance(){
+        return instance;
+    }
+    
+}
+
